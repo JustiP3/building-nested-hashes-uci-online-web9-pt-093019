72c80a07e67653a058571bb404eb4230bf9dd17e
Justin Phillips <justinphil02@yahoo.com>
2019-10-13 23:14:44 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,71 @@
-def third_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
-
+def first_challenge
   
+  #hash level 1 - epic tragedy 
+  
+   epic_tragedy = {
+     #hash level 2 - family names 
+     :montague => {
+       #hash level 3 - each family member 
+       :patriarch => {
+        :name => "Lord Montague" , 
+        :age => "53"
+        },
+        :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+        },
+        :hero => {
+          :name => "Romeo",
+          :age => "15"
+        },
+        #hero friends is an array of hashes representing each friend 
+        :hero_friends => 
+        [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
+        ]
+        },
+        # end of montague family
+        :capulet => {
+          :patriarch => {
+            :name => "Lord Capulet",
+            :age => "50"
+          },
+          :matriarch => {
+            :name => "Lady Capulet",
+            :age => "51"
+          },
+          :heroine => {
+            :name => "Juliet",
+            :age => "15",
+            :status => "alive"
+          },
+          :heroine_friends => [
+            {
+              :name => "Steven",
+              :age => "30",
+              :attitude => "confused"
+            },
+            {
+              :name => "Nurse",
+              :age => "44",
+              :attitude => "worried"
+            }
+            ]
+        } 
+        #end of capulet family
+        
+        
+      }
+     # end of epic tragedy 
+     
+   
 end
