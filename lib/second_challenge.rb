30c673a8f3de2bf5fcc41325fd4c206de32c608e
Justin Phillips <justinphil02@yahoo.com>
2019-10-13 23:31:52 +0000

    Automatically backed up by Learn

---
+++ lib/second_challenge.rb
@@ -0,0 +1,25 @@
+
+def second_challenge
+   epic_tragedy = {
+     #hash level 2 - family names
+     :montague => {
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends =>  []
+        },
+        # end of montague family
+        :capulet => {
+          :patriarch => {},
+          :matriarch => {},
+          :heroine => {},
+          :heroine_friends => []
+        }
+        #end of capulet family
+
+
+      }
+     # end of epic tragedy
+
+
+end
